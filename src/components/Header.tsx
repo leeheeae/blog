604caa05750041fe36344c38dfd23afdfa92e20d
Heeae <chromeee@daum.net>
2023-09-05 21:44:09 +0900

    path alias 설정

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// Styled
+const HeaderContainer = styled.header``;
+
+function Header() {
+  return <HeaderContainer>header</HeaderContainer>;
+}
+
+export default Header;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      HomePage
+    </div>
+  );
+}
+
+export default HomePage;
