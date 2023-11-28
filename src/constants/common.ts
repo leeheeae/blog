0aec49e8755f0ed43899b894b12d6d62a3591a6c
Heeae <chromeee@daum.net>
2023-11-28 17:02:06 +0900

    :rocket: 작업한 내용 deploy, 404 페이지 추가

---
+++ src/constants/common.ts
@@ -0,0 +1,24 @@
+import { LnbMenu } from "@/types/common.type.ts";
+
+export const LNB: LnbMenu[] = [
+  {
+    name: "Home",
+    link: "/"
+  },
+  {
+    name: "Work",
+    link: "/work"
+  },
+  {
+    name: "Portfolio",
+    link: "/Portfolio"
+  },
+  {
+    name: "Blog",
+    link: "/blog"
+  },
+  {
+    name: "Contact",
+    link: "/contact"
+  }
+];
